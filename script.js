8526b51fb8f0bc5787eab4afacf613db789b1cd8
adisamicheal <adisamicheal20@gmail.com>
2020-07-18 10:54:23 +0100

    landing page implementation

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('#hamburger');
+    const menu = document.querySelector('#menu');
+  
+    hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('show');
+      menu.classList.toggle('show');
+    })
+  })
